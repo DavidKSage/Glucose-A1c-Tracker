f6471ae6d3e885e928769b81491504a33f0c6796
David K. Sage <dkstenor@mac.com>
2023-07-15 00:17:19 -0400

    Added render.com database connection string

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,22 +2,22 @@ from flask import Flask
 from config import  Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_restful import Resource, Api, abort, reqparse
+from flask_restful import Api, abort, reqparse
 from flask_marshmallow import Marshmallow
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-load_dotenv()
+# load_dotenv()
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
 migrate = Migrate(app, db)
 cors = CORS(app)
-app.secret_key = os.getenv('SECRET_KEY')
+# app.secret_key = os.getenv('SECRET_KEY')
 
 from app import routes, models
 
